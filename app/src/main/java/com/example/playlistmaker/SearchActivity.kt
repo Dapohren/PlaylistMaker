2cebfba6fb54680b134841ff998a4448137e510d
Angelina Zykova <angelina_zykova@mail.ru>
2023-11-23 17:10:08 +0300

    sprint #9

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,72 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var editText: EditText
+    private lateinit var clearIcon: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        editText = findViewById(R.id.search_button2)
+        clearIcon = findViewById(R.id.clearIcon)
+        val backButton2 = findViewById<Button>(R.id.back_button2)
+        //Кнопочка назад
+
+        backButton2.setOnClickListener {
+            finish()
+        }
+
+        //Отчистить строку
+        clearIcon.setOnClickListener {
+            editText.text.clear()
+            val inputMeth =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMeth?.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButtonVisibility(s?.isNotEmpty() ?: false)
+            }
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+
     }
+
+    private fun clearButtonVisibility(isVisible: Boolean) {
+        clearIcon.visibility = if (isVisible) View.VISIBLE else View.GONE
+    }
+
+
+
+    // Хранение данных
+
+    companion object {
+        const val SEARCH = "TEXT"
+    }
+    private var textString: String = ""
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH, textString)
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        textString = savedInstanceState.getString(SEARCH).toString()
+        editText.setText(textString)
+    }
+
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,10 +1,22 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.res.Configuration
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity: AppCompatActivity() {
+
+
+    @SuppressLint("StringFormatInvalid", "SuspiciousIndentation", "MissingInflatedId")
+    private lateinit var switchDarkMode: SwitchCompat
+    @SuppressLint("StringFormatInvalid", "SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
@@ -13,6 +25,63 @@ class SettingsActivity: AppCompatActivity() {
         backButton.setOnClickListener {
             finish()
         }
+        switchDarkMode = findViewById(R.id.switch_off)
+        switchDarkMode.setOnCheckedChangeListener { _, isChecked ->
+            if (!isChecked) {
+                when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                    Configuration.UI_MODE_NIGHT_YES -> {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                        recreateActivity()
+                    }
+                }
+            } else {
+                when (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+                    Configuration.UI_MODE_NIGHT_NO ->  {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                        recreateActivity()
+                    }
+                }
+
+            }
+        }
+
+        val shareApp: Button = findViewById(R.id.share)
+        shareApp.setOnClickListener {
+            val appId = "com.Practicum.PlaylistMaker"
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+                shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.sharing, appId))
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.share_app_title)))
+        }
+
+        val supportButton: Button = findViewById(R.id.help)
+        supportButton.setOnClickListener {
+            Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse(getString(R.string.recipientEt))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.supportTitle))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.supportText))
+                startActivity(this)
+            }
+        }
+        val agreeButton: Button = findViewById(R.id.agree)
+        agreeButton.setOnClickListener {
+                val textAgree = getString(R.string.agree)
+                val openIntent = Intent(Intent.ACTION_VIEW)
+                openIntent.data = Uri.parse(textAgree)
+                startActivity(openIntent)
+
+
+
+        }
+
+
+
     }
+    
+    private fun recreateActivity() {
+        recreate()
+    }
+
+
 
 }
