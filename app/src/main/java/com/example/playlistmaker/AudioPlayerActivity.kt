d142cafbf366a3bff79ef5c2f3e0c0752580b8a4
Angelina Zykova <angelina_zykova@mail.ru>
2024-02-20 23:26:04 +0300

    #14.1

--- app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/AudioPlayerActivity.kt
@@ -1,10 +1,16 @@
 package com.example.playlistmaker
 
 import android.content.Context
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.res.ResourcesCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.google.gson.Gson
@@ -22,6 +28,22 @@ class AudioPlayerActivity : AppCompatActivity() {
     private lateinit var genre: TextView
     private lateinit var country:TextView
     private lateinit var songTime: TextView
+    private lateinit var url: String
+    private lateinit var play: ImageView
+    private var mediaPlayer = MediaPlayer()
+    private lateinit var handler: Handler
+    private lateinit var timePassedRunnable: Runnable
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+        private const val DELAY = 1000L
+    }
+
+    private var playerState = STATE_DEFAULT
+    private var isPlayerUsed = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,8 +58,10 @@ class AudioPlayerActivity : AppCompatActivity() {
         genre = findViewById(R.id.genreName)
         country = findViewById(R.id.countryName)
         songTime = findViewById(R.id.timeNum)
+        play = findViewById(R.id.buttonPlay)
         lateinit var chosenTrack : DataSongs
         val extras: Bundle? = intent.extras
+        handler = Handler(Looper.getMainLooper())
 
 
         backButton.setOnClickListener {
@@ -62,9 +86,78 @@ class AudioPlayerActivity : AppCompatActivity() {
         year.text = chosenTrack.releaseDate.take(4)
         genre.text = chosenTrack.primaryGenreName
         country.text = chosenTrack.country
-        songTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(chosenTrack.trackTimeMillis)
+        url = chosenTrack.previewUrl
 
 
+        play.setOnClickListener {
+            playbackControl()
+        }
+
+        preparePlayer()
+    }
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+        if(isPlayerUsed){
+            handler.removeCallbacks(timePassedRunnable)
+        }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+        if(isPlayerUsed){
+            handler.removeCallbacks(timePassedRunnable)
+        }
+    }
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            play.isEnabled = true
+            playerState = STATE_PREPARED
+            if(isPlayerUsed){
+                handler.removeCallbacks(timePassedRunnable)
+            }
+            songTime.text = getString(R.string.time)
+        }
+        mediaPlayer.setOnCompletionListener {
+            playerState = STATE_PREPARED
+            play.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.play, theme))
+        }
+    }
+    private fun startPlayer() {
+        mediaPlayer.start()
+        playerState = STATE_PLAYING
+        play.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.pause, theme))
+        if(!isPlayerUsed){
+            timePassedRunnable = createTimePassedTask()
+        }
+        handler.post(timePassedRunnable)
+        isPlayerUsed = true
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        playerState = STATE_PAUSED
+        play.setImageDrawable(ResourcesCompat.getDrawable(resources, R.drawable.play, theme))
+    }
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+    private fun createTimePassedTask(): Runnable {
+        return object : Runnable {
+            override fun run() {
+                songTime.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(mediaPlayer.currentPosition)
+                handler.postDelayed(this, DELAY)
+            }
+        }
     }
 }
 
